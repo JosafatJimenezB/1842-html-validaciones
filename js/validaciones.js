17fedcf5b63f1eb42927cea7aec6be4fc2874a5b
josafat <josa221001@gmail.com>
2023-03-13 22:34:45 -0600

    add: validacion del numero de telefono

--- js/validaciones.js
+++ js/validaciones.js
@@ -23,19 +23,23 @@ const tipoDeErrores = [
 
 const mensajesDeError = {
     nombre: {
-        valueMissing: "Este campo no puede estar vacío",
+        valueMissing: "El campo nombre no puede estar vacío",
     },
     email: {
-        valueMissing: "Este campo no puede estar vacío",
+        valueMissing: "El campo email no puede estar vacío",
         typeMismatch: "El correo no es válido"
     },
     password: {
-        valueMissing: "Este campo no puede estar vacío",
+        valueMissing: "El campo password no puede estar vacío",
         patternMismatch: "Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales.",
     },
     nacimiento: {
-        valueMissing: "Este campo no puede estar vacío",
+        valueMissing: "El campo nacimiento no puede estar vacío",
         customError: "Debes tener al menos 18 años de edad"
+    },
+    numero: {
+        valueMissing: "El campo número no puede estar vacío",
+        patternMismatch: "El formato requerido es XXXXXXXXXX",
     }
 }
 
